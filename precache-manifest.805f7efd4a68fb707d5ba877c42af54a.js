297ea4671902f498b47d67ca0bc35d675ce8c135
Viktoria Gordeeva <viktoria71311@gmail.com>
2020-03-30 17:33:10 +0300

    Updates

---
+++ precache-manifest.805f7efd4a68fb707d5ba877c42af54a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc8f53efc887a050655cc4ebacebe6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b17262336bf2c224c4ff",
+    "url": "/static/css/main.4b17994f.chunk.css"
+  },
+  {
+    "revision": "187070f44f2ffcf16541",
+    "url": "/static/js/2.eef7b601.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.eef7b601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17262336bf2c224c4ff",
+    "url": "/static/js/main.44b8c426.chunk.js"
+  },
+  {
+    "revision": "c86bda5beec795370e44",
+    "url": "/static/js/runtime-main.013b9dec.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
